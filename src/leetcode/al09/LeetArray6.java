366b02b6d3f5b64a7bdb5e56703c023d46b47039
JisooPyo <audwl326@gmail.com>
2023-09-06 10:28:37 +0900

    leetcode: Remove Element

---
+++ src/leetcode/al09/LeetArray6.java
@@ -0,0 +1,25 @@
+package leetcode.al09;
+
+public class LeetArray6 {
+	public static void main(String[] args) {
+		LeetArray6 la = new LeetArray6();
+		int[] nums1 = {3, 2, 2, 3};
+		int[] nums2 = {0, 1, 2, 2, 3, 0, 4, 2};
+		System.out.println(la.removeElement(nums1, 3));
+		System.out.println(la.removeElement(nums2, 2));
+	}
+
+	public int removeElement(int[] nums, int val) {
+		int count = nums.length;
+		int index = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == val) {
+				count--;
+			} else {
+				nums[index] = nums[i];
+				index++;
+			}
+		}
+		return count;
+	}
+}
