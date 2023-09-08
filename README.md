3c87f751d2514df1529e0db7ab956b30386c6af6
JisooPyo <audwl326@gmail.com>
2023-09-08 23:35:52 +0900

    Programmers: 짝지어 제거하기

--- README.md
+++ README.md
@@ -2,18 +2,28 @@
 
 ## Programmers
 
+<details>
+<summary>
 기초 트레이닝, 입문 완료
+</summary>
 
 | ![코딩 기초 트레이닝 캘린더](https://github.com/JisooPyo/My-Study/assets/130378232/ecdaf685-54e7-415f-8194-62d2a131a668) | ![코딩테스트 입문 캘린더 (1)](https://github.com/JisooPyo/My-Study/assets/130378232/4daaedbf-d6c0-40a1-a8f1-acb7cd183f8e) |
 |---------------------------------------------------------------------------------------------------------------|-----------------------------------------------------------------------------------------------------------------|
 
-Programmers lv.1 완료
-
-![image](https://github.com/JisooPyo/My-Study/assets/130378232/fdd15c4e-d3c0-4a26-9305-e5e0cb563b9b)
+</details>
 
 <details>
 <summary>
-Programmers lv.2
+Programmers lv.1 완료
+</summary>
+
+![image](https://github.com/JisooPyo/My-Study/assets/130378232/fdd15c4e-d3c0-4a26-9305-e5e0cb563b9b)
+
+</details>
+
+<details>
+<summary>
+Programmers lv.2 풀이 중
 </summary>
 
 * 최댓값과 최솟값
@@ -24,6 +34,7 @@ Programmers lv.2
 * 숫자의 표현
 * 다음 큰 숫자
 * 피보나치 수
+* 짝지어 제거하기
 
 </details>
 
---
+++ src/leetcode/al09/ProRemovingInPairs.java
@@ -0,0 +1,62 @@
+// 짝지어 제거하기
+package leetcode.al09;
+
+import java.util.Stack;
+
+public class ProRemovingInPairs {
+	public static void main(String[] args) {
+		ProRemovingInPairs rip = new ProRemovingInPairs();
+		System.out.println(rip.solution("iifedcbaabcdefgghh"));
+		System.out.println(rip.solution("cdcd"));
+		System.out.println(rip.solution("baabaa"));
+	}
+
+	// 문자열의 길이 최대 1,000,000
+	public int solution(String s) {
+		Stack<Character> stack = new Stack<>();
+		for (int i = 0; i < s.length(); i++) {
+			if ((stack.size() > 0) && (stack.peek().equals(s.charAt(i)))) {
+				stack.pop();
+			} else {
+				stack.add(s.charAt(i));
+			}
+		}
+		return stack.size() > 0 ? 0 : 1;
+	}
+}
+
+/*
+
+정확성  테스트
+
+테스트 1 〉통과 (0.28ms, 78.3MB)
+테스트 2 〉통과 (20.09ms, 74.7MB)
+테스트 3 〉통과 (19.80ms, 81.2MB)
+테스트 4 〉통과 (13.76ms, 79.1MB)
+테스트 5 〉통과 (16.16ms, 75.2MB)
+테스트 6 〉통과 (20.11ms, 83.9MB)
+테스트 7 〉통과 (30.35ms, 79.2MB)
+테스트 8 〉통과 (17.46ms, 76.5MB)
+테스트 9 〉통과 (0.27ms, 73.8MB)
+테스트 10 〉통과 (0.32ms, 76.7MB)
+테스트 11 〉통과 (0.17ms, 74.3MB)
+테스트 12 〉통과 (0.20ms, 73.5MB)
+테스트 13 〉통과 (0.33ms, 75.3MB)
+테스트 14 〉통과 (0.19ms, 76.3MB)
+테스트 15 〉통과 (0.19ms, 71.1MB)
+테스트 16 〉통과 (0.31ms, 80.8MB)
+테스트 17 〉통과 (0.27ms, 74.5MB)
+테스트 18 〉통과 (0.20ms, 72.2MB)
+
+효율성  테스트
+
+테스트 1 〉통과 (68.67ms, 61.9MB)
+테스트 2 〉통과 (40.98ms, 56MB)
+테스트 3 〉통과 (49.66ms, 59MB)
+테스트 4 〉통과 (52.74ms, 58.7MB)
+테스트 5 〉통과 (50.33ms, 59.5MB)
+테스트 6 〉통과 (49.46ms, 58.8MB)
+테스트 7 〉통과 (50.93ms, 58.6MB)
+테스트 8 〉통과 (49.91ms, 58.5MB)
+
+ */
