c5cf39bf46b62afaeef575867b9ccceb9d9b26bf
JisooPyo <audwl326@gmail.com>
2023-09-30 15:51:36 +0900

    Leetcode: Move Zeroes

---
+++ src/leetcode/al09/LeetArray12.java
@@ -0,0 +1,32 @@
+package leetcode.al09;
+
+import java.util.Arrays;
+
+public class LeetArray12 {
+	public static void main(String[] args) {
+		LeetArray12 la12 = new LeetArray12();
+
+		int[] nums1 = {0, 1, 0, 3, 12};
+		int[] nums2 = {0};
+
+		la12.moveZeroes(nums1);
+		la12.moveZeroes(nums2);
+
+		System.out.println(Arrays.toString(nums1));
+		System.out.println(Arrays.toString(nums2));
+	}
+
+	public void moveZeroes(int[] nums) {
+		int index = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] != 0) {
+				nums[index] = nums[i];
+				index++;
+			}
+		}
+
+		for (int i = index; i < nums.length; i++) {
+			nums[i] = 0;
+		}
+	}
+}
