4312f7c7509fd225a24d77c7a5ba6bb2ab2f85f8
JisooPyo <audwl326@gmail.com>
2024-02-23 11:16:10 +0900

    Leetcode: 35. Search Insert Position

---
+++ src/leetcode/al2402/Leet35.java
@@ -0,0 +1,12 @@
+package leetcode.al2402;
+
+public class Leet35 {
+    public int searchInsert(int[] nums, int target) {
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == target || nums[i] > target){
+                return i;
+            }
+        }
+        return nums.length;
+    }
+}
