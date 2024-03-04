e0ebb73c69d7426c0f4eda0b23e8a15d84edb19f
JisooPyo <audwl326@gmail.com>
2024-03-04 19:57:55 +0900

    Leetcode: 948. Bag of Tokens

---
+++ src/leetcode/al2403/Leet948.java
@@ -0,0 +1,33 @@
+package leetcode.al2403;
+
+import java.util.Arrays;
+
+public class Leet948 {
+    public static void main(String[] args) {
+        Leet948 l = new Leet948();
+    }
+
+    public int bagOfTokensScore(int[] tokens, int power) {
+        Arrays.sort(tokens);
+        int s = 0;
+        int maxi = 0;
+        int l = 0, r = tokens.length - 1;
+
+        while (l <= r) {
+            if (power >= tokens[l]) {
+                power -= tokens[l];
+                s++;
+                l++;
+                maxi = Math.max(maxi, s);
+            } else if (s > 0) {
+                power += tokens[r];
+                s--;
+                r--;
+            } else {
+                break;
+            }
+        }
+
+        return maxi;
+    }
+}
