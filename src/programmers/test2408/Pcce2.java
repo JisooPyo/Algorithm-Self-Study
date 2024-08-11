edbc6db531e2374b9de6838d4aae7846d24e5717
JisooPyo <audwl326@gmail.com>
2024-08-11 20:14:43 +0900

    Programmers: [PCCE 기출문제] 2번 / 피타고라스의 정리

---
+++ src/programmers/test2408/Pcce2.java
@@ -0,0 +1,15 @@
+package programmers.test2408;
+
+import java.util.Scanner;
+
+public class Pcce2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int c = sc.nextInt();
+
+        int b_square = c * c - a * a;
+
+        System.out.println(b_square);
+    }
+}
