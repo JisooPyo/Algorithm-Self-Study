70b91a8d49deff19801ea916be1aef632c9e7d08
JisooPyo <audwl326@gmail.com>
2024-08-11 20:24:29 +0900

    Programmers: [PCCE 기출문제] 6번 / 가채점

---
+++ src/programmers/test2408/Pcce6.java
@@ -0,0 +1,18 @@
+package programmers.test2408;
+
+public class Pcce6 {
+    public String[] solution(int[] numbers, int[] our_score, int[] score_list) {
+        int num_student = numbers.length;
+        String[] answer = new String[num_student];
+
+        for (int i = 0; i < num_student; i++) {
+            if (our_score[i] == score_list[numbers[i] - 1]) {
+                answer[i] = "Same";
+            } else {
+                answer[i] = "Different";
+            }
+        }
+
+        return answer;
+    }
+}
