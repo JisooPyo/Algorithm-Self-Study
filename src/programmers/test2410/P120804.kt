d83ef07f62a9fe6156edf992cb9cc8716e43238b
JisooPyo <audwl326@gmail.com>
2024-10-12 00:36:39 +0900

    Programmers: 두 수의 곱

---
+++ src/programmers/test2410/P120804.kt
@@ -0,0 +1,11 @@
+package programmers.test2410
+
+fun main() {
+    val p = P120804()
+    println(p.solution(3, 4))       // 12
+    println(p.solution(27, 19))     // 513
+}
+
+class P120804 {
+    fun solution(num1: Int, num2: Int) = num1 * num2
+}
