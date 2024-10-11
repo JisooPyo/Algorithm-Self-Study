f83a408497e10d0f76c1002d0fcab7318ac0eebf
JisooPyo <audwl326@gmail.com>
2024-10-12 00:31:51 +0900

    Programmers: 숫자 비교하기

---
+++ src/programmers/test2410/P120807.kt
@@ -0,0 +1,12 @@
+package programmers.test2410
+
+fun main() {
+    val p = P120807()
+    println(p.solution(2, 3))   // -1
+    println(p.solution(11, 11))   // 1
+    println(p.solution(7, 99))   // -1
+}
+
+class P120807 {
+    fun solution(num1: Int, num2: Int): Int = if (num1 == num2) 1 else -1
+}
