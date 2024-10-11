827c7bd4185ab3eb9e2a7ce2c527e17f6c0d9505
JisooPyo <audwl326@gmail.com>
2024-10-12 00:40:37 +0900

    Programmers: 두 수의 나눗셈

---
+++ src/programmers/test2410/P120806.kt
@@ -0,0 +1,14 @@
+package programmers.test2410
+
+fun main() {
+    val p = P120806()
+    println(p.solution(3, 2))   // 1500
+    println(p.solution(7, 3))   // 2333
+    println(p.solution(1, 16))   // 62
+}
+
+class P120806 {
+    fun solution(num1: Int, num2: Int): Int {
+        return ((num1.toDouble() / num2) * 1000).toInt()
+    }
+}
