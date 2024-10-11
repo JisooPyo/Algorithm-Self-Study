6668f993df8e532eef84fd5c17a8c0b0c9632a8b
JisooPyo <audwl326@gmail.com>
2024-10-12 00:34:27 +0900

    Programmers: 몫 구하기

---
+++ src/programmers/test2410/P120805.kt
@@ -0,0 +1,11 @@
+package programmers.test2410
+
+fun main() {
+    val p = P120805()
+    println(p.solution(10, 5))      // 2
+    println(p.solution(7, 2))       // 3
+}
+
+class P120805 {
+    fun solution(num1: Int, num2: Int): Int = num1 / num2
+}
