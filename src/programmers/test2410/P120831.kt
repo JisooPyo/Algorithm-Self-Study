059647dedd59a715c9f6c19bc04323fbfadbbb72
JisooPyo <audwl326@gmail.com>
2024-10-18 09:11:53 +0900

    Programmers(Kotlin): 짝수의 합

---
+++ src/programmers/test2410/P120831.kt
@@ -0,0 +1,21 @@
+package programmers.test2410
+
+/**
+ * 프로그래머스 lv.0 짝수의 합
+ */
+fun main() {
+    println(solution(10))   // 30
+    println(solution(4))    // 6
+}
+
+fun solution(n: Int): Int {
+    var answer = 0
+
+    for (i in 1..n / 2) {
+        answer += i * 2
+    }
+
+    return answer
+}
+
+fun solutionOther(n: Int) = (0..n).filter { it % 2 == 0 }.sum()
