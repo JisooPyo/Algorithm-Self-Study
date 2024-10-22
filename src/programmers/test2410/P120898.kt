4d2daa54a4d4388f6a4b31ce3771f4e0aefdda18
JisooPyo <audwl326@gmail.com>
2024-10-22 22:05:09 +0900

    Programmers(Kotlin): 편지

---
+++ src/programmers/test2410/P120898.kt
@@ -0,0 +1,15 @@
+package programmers.test2410
+
+/**
+ * 프로그래머스 lv.0 편지
+ */
+fun main() {
+    val p = P120898()
+    println(p.solution("happy birthday!"))  // 30
+    println(p.solution("i love you~"))      // 22
+
+}
+
+class P120898 {
+    fun solution(message: String) = message.length * 2
+}
