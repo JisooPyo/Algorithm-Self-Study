33eceb827a9ac6d70d49b695f86c870e659b6246
JisooPyo <audwl326@gmail.com>
2024-12-08 16:30:54 +0900

    Leetcode 풀이 코드 템플릿 생성

---
+++ src/leetcode/al2412/Example.kt
@@ -0,0 +1,27 @@
+package leetcode.al2412
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+/**
+ * Leetcode
+ * {No}. {Problem}
+ * {Difficulty}
+ */
+class Example {
+    /**
+     * Runtime: {Time} ms(Beats: {Percent} %)
+     * Time Complexity: O()
+     *
+     * Memory: {Memory} MB(Beats: {Percent} %)
+     * Space Complexity: O()
+     */
+    fun example(): Int {
+        TODO()
+    }
+
+    @Test
+    fun test() {
+        example() shouldBe 0
+    }
+}
